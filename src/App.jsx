37f35576230116a93f0e6c8270a836c137649e1d
Lxvia <oliviarudolf67@gmail.com>
2025-02-28 16:27:36 +0100

    conflits ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TemporaryAccueil from "./pages/TemporaryAccueil";
+import Home from "./pages/Home";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<TemporaryAccueil />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   )
